3b79b61fe6872379be089ca66af765c12b17a850
Antonio <antonio11duran@gmail.com>
2022-10-29 16:48:19 -0600

    path fixes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-
 const notesRouter = require('./notes');
 
 const app = express();
 
-app.use('/notes', notesRouter);
+app.use('/api/notes', notesRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 // const { clog } = require('./middleware/clog');
-const api = require('./routes/index');
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,17 +10,18 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+app.get('*', (req, res) =>
+res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
+
+require('./routes/index')(app);
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
