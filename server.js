7b29a1a633ac0f2b15d3f740f81e64307ebd5144
Antonio <antonio11duran@gmail.com>
2022-10-29 17:11:56 -0600

    path

--- server.js
+++ server.js
@@ -11,17 +11,18 @@ app.use(clog);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
 app.use(express.static('public'));
 
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
+res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
+
+app.use('/api', api);
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
