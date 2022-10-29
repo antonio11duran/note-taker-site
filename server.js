4636a005cc8e5fd35bd1aa06bf38958dc1a8e453
Antonio <antonio11duran@gmail.com>
2022-10-29 15:21:02 -0600

    heroku fix

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const path = require('path');
 const { clog } = require('./middleware/clog');
 const api = require('./routes/index');
 
-const PORT = process.env.PORT || 3001;
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 const app = express();
 
