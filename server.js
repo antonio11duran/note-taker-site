2e74a327cc32730aa15cb9073243480b24912c9f
Duran11 <antonio11duran@gmail.com>
2022-10-31 10:17:50 -0600

    moved /api

--- server.js
+++ server.js
@@ -11,18 +11,19 @@ app.use(clog);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
 app.use(express.static('public'));
 
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '../public/index.html'))
+res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
+res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
 
 app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT} 🚀`)
+console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
+
+app.use('/api', api);
