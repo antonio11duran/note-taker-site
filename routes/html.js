2a50770909ae0bdf2b00eb7e29bb3a6e9d3973ce
Duran11 <antonio11duran@gmail.com>
2022-10-31 12:10:18 -0600

    added / to get stuff to move from homepage

--- routes/html.js
+++ routes/html.js
@@ -5,6 +5,10 @@ html.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
 
+html.get('/', (req, res) =>
+res.sendFile(path.join(__dirname, '../public/index.html'))
+);
+
 html.get('*', (req, res) =>
 res.sendFile(path.join(__dirname, '../public/index.html'))
 );
