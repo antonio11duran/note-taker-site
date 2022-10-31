0b7c311c142b67f0bae33ffb2acdd591038e365a
Duran11 <antonio11duran@gmail.com>
2022-10-31 12:11:55 -0600

    removed *

--- routes/html.js
+++ routes/html.js
@@ -9,8 +9,4 @@ html.get('/', (req, res) =>
 res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
-html.get('*', (req, res) =>
-res.sendFile(path.join(__dirname, '../public/index.html'))
-);
-
 module.exports = html;
