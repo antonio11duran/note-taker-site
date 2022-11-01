e313aa07b248cddcf70f3bca1f1e21b5e886ee06
Duran11 <antonio11duran@gmail.com>
2022-10-31 20:55:42 -0600

    new attempt

--- routes/html.js
+++ routes/html.js
@@ -5,7 +5,7 @@ html.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
 
-html.get('*', (req, res) =>
+html.get('/*', (req, res) =>
 res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
